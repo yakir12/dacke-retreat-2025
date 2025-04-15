2d8cb1ad66776f21f145c684530946061c4159e2
Yakir Gagnon <12.yakir@gmail.com>
2025-04-15 11:31:09 +0200

    wip

--- main.jl
+++ main.jl
@@ -1,8 +1,8 @@
 using GLMakie, AlgebraOfGraphics, Distributions, DataFrames, Chain, DataFramesMeta
 using GLM, CoordinateTransformations, Rotations
 
-function model(x, a, b)
-    μ = a*x + b
+function model(x, slope, intercept)
+    μ = slope*x + intercept
     # identity(μ)
 end
 
@@ -10,51 +10,53 @@ sample1 = rand ∘ Normal
 
 n = 100
 x = 10rand(n)
-a, b = (5, 15)
+slope, intercept = (5, 15)
 σ = 2
 
+Random.seed!(0)
 df = DataFrame(x = x)
 @chain df begin
-    @transform! :y0 = model.(:x, a, b)
-    @transform! :y = sample1.(:y0, σ)
+    @transform! :parameters = model.(:x, slope, intercept)
+    @transform! :measurement = sample1.(:parameters, σ)
 end
 
 fig = Figure()
 ax1 = Axis(fig[1,1], aspect = 1, xlabel = "x")
 hist!(ax1, df.x)
-ax2 = Axis(fig[1,2], aspect = 1, xlabel = "y")
-hist!(ax2, df.y)
+ax2 = Axis(fig[1,2], aspect = 1, xlabel = "measurement")
+hist!(ax2, df.measurement)
 ax3 = Axis(fig[1,3], aspect = 1, xlabel = "ϵ")
-hist!(ax3, df.y0 - df.y)
+hist!(ax3, df.parameters - df.measurement)
 
-data(df) * (mapping(:x, :y0) * visual(Lines; color = :red, label = "$a*x + $b") + mapping(:x, :y) * visual(Scatter; label = "noisy data")) |> draw()
+data(df) * (mapping(:x, :parameters) * visual(Lines; color = :red, label = "$slope*x + $intercept") + mapping(:x, :measurement) * visual(Scatter; label = "noisy data")) |> draw()
 
-m = lm(@formula(y ~ x), df)
-b̂, â = coef(m)
+m = lm(@formula(measurement ~ x), df)
+fitted_intercept, fitted_slope = coef(m)
 
+Random.seed!(0)
 @chain df begin
-    @transform! :ŷ = â * :x .+ b̂
-    @transform! :ϵ̂ = :y .- :ŷ
+    @transform! :fitted_parameters = model.(:x, fitted_slope, fitted_intercept)
+    @transform! :ϵ̂ = :measurement .- :fitted_parameters
 end
 
-@assert df.ŷ == predict(m)
+@assert df.fitted_parameters == predict(m)
 @assert df.ϵ̂ == residuals(m)
 
 d = Distributions.fit(Normal, residuals(m))
 
 fig = Figure()
-ax = Axis(fig[1,1], xlabel = "x", ylabel = "y")
-lines!(ax, df.x, df.y0, label = "phenomenon", color = :black)
+ax = Axis(fig[1,1], xlabel = "x", ylabel = "measurement")
+lines!(ax, df.x, df.parameters, label = "parameters", color = :black)
 
-scatter!(ax, df.x, df.y, label = "measurements", color = :green)
+scatter!(ax, df.x, df.measurement, label = "measurements", color = :green)
 
-lines!(ax, df.x, df.ŷ, label = "fit", color = :red)
+lines!(ax, df.x, df.fitted_parameters, label = "fit", color = :red)
 
-rangebars!(ax, df.x, df.y, df.ŷ, label = "residuals", color = :gray)
+rangebars!(ax, df.x, df.measurement, df.fitted_parameters, label = "residuals", color = :gray)
 axislegend(ax, position = :lt)
 
 function generate(x)
-    μ = model(x, a, b)
+    μ = model(x, slope, intercept)
     sample1(μ, σ)
 end
 
@@ -62,8 +64,8 @@ scatter!(ax, x, generate.(x))
 
 ####
 
-function model(x, a, b)
-    μ = a*x + b
+function model(x, slope, intercept)
+    μ = slope*x + intercept
     GLM.linkinv.(LogitLink(), μ)
 end
 
@@ -71,23 +73,30 @@ sample1 = rand ∘ Bernoulli
 
 n = 300
 x = 5sort(rand(n) .- 0.5)
-a, b = (2, 1)
+slope, intercept = (2, 1)
 
-Random.seed!(1234)
+Random.seed!(0)
 df = DataFrame(x = x)
 @chain df begin 
-    @transform! :y0 = model.(:x, a, b)
+    @transform! :y0 = model.(:x, slope, intercept)
     @transform! :success = sample1.(:y0)
 end
 
-data(df) * mapping(:x, :success => Float64) * visual(Scatter; label = "noisy data") |> draw()
+data(df) * mapping(:x, :success) * visual(Scatter; label = "noisy data") |> draw()
 
 m = glm(@formula(success ~ x), df, Binomial(), LogitLink())
 
-b̂, â = coef(m)
+fitted_intercept, fitted_slope = coef(m)
 
 df.y = predict(m)
 
 data(df) * (mapping(:x, :y0) * visual(Lines; label = "model") + mapping(:x, :y) * visual(Lines; color = :red, label = "fitted")) |> draw()
 
-Random.seed!(1234)
+Random.seed!(0)
+@chain df begin 
+    @transform! :y0_predicted = model.(:x, fitted_slope, fitted_intercept)
+    @transform! :success_predicted = sample1.(:y0_predicted)
+    @transform! :same = :success_predicted .== :success
+end
+
+data(df) * mapping(:x, :success, color = :same) * visual(Scatter; alpha = 0.5) |> draw()
