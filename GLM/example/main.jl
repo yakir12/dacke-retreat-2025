a84616e4c0b3db05a434c02e60185c45632b64b9
Yakir Gagnon <12.yakir@gmail.com>
2025-04-27 18:29:02 +0200

    wip

--- GLM/example/main.jl
+++ GLM/example/main.jl
@@ -1,64 +1,107 @@
-using MixedModels, GLM, GLMakie, DataFrames, AlgebraOfGraphics
+using MixedModels, GLM, GLMakie, DataFrames, AlgebraOfGraphics, Distributions
 
-n = 100
-ngroups = 6
-global_intercept = 1
-global_slope = 1
+n = 50
+ngroups = 10
+intercept = 5
+slope = 10
+σ = .2
 
-individual_intercepts = randn(ngroups)
-individual_slopes = randn(ngroups)
+individual_intercepts = 3randn(ngroups)
+individual_slopes = 3randn(ngroups)
 
-function random_effect(id, intercept_factor, slope_factor)
-    intercept = global_intercept + intercept_factor * individual_intercepts[id]
-    slope = global_slope + slope_factor * individual_slopes[id]
-    return (intercept, slope)
+df0 = DataFrame(x = rand(n), id = rand(1:ngroups, n))
+ndf2 = flatten(combine(groupby(df0, :id), :x => extrema => :x), :x)
+ndf2.y .= 0.0
+ndf2pop = DataFrame(x = 0:1, y = zeros(2), id = fill(ngroups + 1, 2))
+ndf3 = flatten(combine(groupby(df0, :id), :x => extrema => :x), :x)
+ndf3.y .= 0.0
+ndf3pop = DataFrame(x = 0:1, y = zeros(2), id = fill(ngroups + 1, 2))
+
+random_effects(id, intercept_factor, slope_factor) = (intercept_factor * individual_intercepts[id], slope_factor * individual_slopes[id])
+model(intercept, slope, x) = intercept + slope*x
+measure(μ) = rand(Normal(μ, σ))
+function sample1(x, id, intercept_factor, slope_factor)
+    individual_intercept, individual_slope = random_effects(id, intercept_factor, slope_factor)
+    μ = model(individual_intercept + intercept, individual_slope + slope, x)
+    measure(μ)
 end
-
-intercept_factor = 0.9
-slope_factor = 0.9
-df = DataFrame(x = rand(n), id = rand(1:ngroups, n))
-transform!(df, :id => ByRow(id -> random_effect(id, intercept_factor, slope_factor)) => [:intercept, :slope])
-transform!(df, [:x, :intercept, :slope] => ByRow((x, intercept, slope) -> intercept + slope*x + randn()/10) => :y)
-
-m1 = lm(@formula(y ~ 1 + x), df)
-ndf1 = DataFrame(x = 0:1, y = zeros(2))
-ndf1.y .= predict(m1, ndf1)
 
 fig = Figure()
-ax1 = Axis(fig[1,1], title = string("P = ", round(ftest(m1.model).pval, digits = 2)))
-scatter!(ax1, df.x, df.y)
-lines!(ndf1.x, ndf1.y)
+sg = SliderGrid(fig[1, 1:3], (label = "intercept factor", range = 0:0.1:10, startvalue = 0), (label = "slope factor", range = 0:0.1:10, startvalue = 0), tellheight = false)
 
-m2 = fit(MixedModel, @formula(y ~ 1 + x + (1|id)), df)
-ndf2 = flatten(combine(groupby(df, :id), :x => extrema => :x), :x)
-ndf2.y .= 0.0
-ndf2.y .= predict(m2, ndf2)
-ndf2pop = DataFrame(x = 0:1, y = zeros(2), id = fill(ngroups + 1, 2))
-ndf2pop.y .= predict(m2, ndf2pop, new_re_levels=:population)
+intercept_factor, slope_factor = [s.value for s in sg.sliders]
 
-ax2 = Axis(fig[1,2], title = string("P = ", round(m2.pvalues[2], digits = 2)))
-scatter!(ax2, df.x, df.y, color = df.id)
-for g in groupby(ndf2, :id)
-    lines!(g.x, g.y, colorrange = (1, ngroups), color = g.id)
+df = @lift transform(df0, [:x, :id] => ByRow((x, id) -> sample1(x, id, $intercept_factor, $slope_factor)) => :y)
+
+datapoints = @lift Point2.($df.x, $df.y)
+
+m1 = @lift lm(@formula(y ~ 1 + x), $df)
+
+lmline = map(m1) do m1
+    ndf = DataFrame(x = 0:1, y = zeros(2))
+    ndf.y .= predict(m1, ndf)
+    Point2.(ndf.x, ndf.y)
 end
-lines!(ndf2pop.x, ndf2pop.y, colorrange = (1, ngroups), color = :black)
 
+m1p = @lift ftest($m1.model).pval
 
-m3 = fit(MixedModel, @formula(y ~ 1 + x + (1 + x|id)), df)
-ndf3 = flatten(combine(groupby(df, :id), :x => extrema => :x), :x)
-ndf3.y .= 0.0
-ndf3.y .= predict(m3, ndf3)
-ndf3pop = DataFrame(x = 0:1, y = zeros(2), id = fill(ngroups + 1, 2))
-ndf3pop.y .= predict(m3, ndf3pop, new_re_levels=:population)
+m2 = @lift fit(MixedModel, @formula(y ~ 1 + x + (1|id)), $df)
 
-ax3 = Axis(fig[1,3], title = string("P = ", round(m3.pvalues[2], digits = 2)))
-scatter!(ax3, df.x, df.y, colorrange = (1, ngroups), color = df.id)
-for g in groupby(ndf3, :id)
-    lines!(g.x, g.y, colorrange = (1, ngroups), color = g.id)
+rand_inter_lines = map(m2) do m2
+    ndf2.y .= predict(m2, ndf2)
+    combine(groupby(ndf2, :id), [:x, :y] => ((x, y) -> Pair(Point2.(x, y)...)) => :segments).segments
 end
-lines!(ndf3pop.x, ndf3pop.y, color = :black)
+
+ndf2pop_line = map(m2) do m2
+    ndf2pop.y .= predict(m2, ndf2pop, new_re_levels=:population)
+    Point2.(ndf2pop.x, ndf2pop.y)
+end
+
+m2p = @lift $m2.pvalues[2]
+
+m3 = @lift fit(MixedModel, @formula(y ~ 1 + x + (1+x|id)), $df)
+
+rand_inter_slope_lines = map(m3) do m3
+    ndf3.y .= predict(m3, ndf3)
+    combine(groupby(ndf3, :id), [:x, :y] => ((x, y) -> Pair(Point2.(x, y)...)) => :segments).segments
+end
+
+ndf3pop_line = map(m3) do m3
+    ndf3pop.y .= predict(m3, ndf3pop, new_re_levels=:population)
+    Point2.(ndf3pop.x, ndf3pop.y)
+end
+
+m3p = @lift $m3.pvalues[2]
+
+ax1 = Axis(fig[2,1], title = @lift(string("P = ", round($m1p, digits = 2))))
+ablines!(ax1, intercept, slope, color = :gray)
+scatter!(ax1, datapoints)
+lines!(ax1, lmline)
+
+ax2 = Axis(fig[2,2], title = @lift(string("P = ", round($m2p, digits = 2))))
+ablines!(ax2, intercept, slope, color = :gray)
+scatter!(ax2, datapoints, color = @lift($df.id), colorrange = (1, ngroups))
+linesegments!(ax2, rand_inter_lines, color = 1:ngroups, colorrange = (1, ngroups))
+lines!(ax2, ndf2pop_line, color = :black)
+
+ax3 = Axis(fig[2,3], title = @lift(string("P = ", round($m3p, digits = 2))))
+ablines!(ax3, intercept, slope, color = :gray)
+scatter!(ax3, datapoints, color = @lift($df.id), colorrange = (1, ngroups))
+linesegments!(ax3, rand_inter_slope_lines, color = 1:ngroups, colorrange = (1, ngroups))
+lines!(ax3, ndf3pop_line, color = :black)
 
 linkaxes!(ax1, ax2)
 linkaxes!(ax2, ax3)
 
-fig
+on(datapoints) do _
+    autolimits!(ax1)
+end
+
+
+display(fig)
+
+
+
+
+
+
