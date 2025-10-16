12539a772d8899cc0f1ef254f65ccdee1a983f9b
Yakir Gagnon <12.yakir@gmail.com>
2025-10-16 13:17:29 +0200

    last

--- GLM/example/old/handedness.jl
+++ GLM/example/old/handedness.jl
@@ -1,4 +1,53 @@
-using MixedModels, Distributions, DataFrames
+using MixedModels, Distributions, DataFrames, GLM
+
+nindividuals = 10
+n = 10
+df = DataFrame(id = 1:nindividuals)
+df.p = rand(nindividuals)
+# df.p .= 0.5
+transform!(df, :p => ByRow(p -> rand(Bernoulli(p), n)) => :success)
+df = flatten(df, :success)
+df2 = combine(groupby(df, :id), :success => count => :n, nrow)
+transform!(df2, [:nrow, :n] => ByRow(\) => :p)
+
+m = fit(MixedModel, @formula(success ~ 1 + (1 | id)), df, Binomial())
+
+m = glm(@formula(p ~ 1), df2, Binomial(), wts = df2.nrow)
+
+
+n = 6
+m = glm(@formula(p ~ 1), DataFrame(p = rand(n)), Binomial(), wts = fill(2, n))
+
+using HypothesisTests
+
+nindividuals = 10
+n = 10
+df = DataFrame(id = 1:nindividuals)
+# df.p = rand(nindividuals)
+df.p .= 0.5
+transform!(df, :p => ByRow(p -> rand(Bernoulli(p), n)) => :success)
+df = flatten(df, :success)
+df2 = combine(groupby(df, :id), :success => count => :n, nrow)
+transform!(df2, [:nrow, :n] => ByRow((nrow, n) -> n < nrow/2 ? nrow - n : n) => :n)
+transform!(df2, [:n, :nrow] => ByRow((n, nrow) -> confint(BinomialTest(n, nrow), tail = :both)) => :p)
+
+# transform!(df2, [:nrow, :n] => ByRow(\) => :p)
+
+using GLMakie
+nindividuals = 10
+n = 10
+df = DataFrame(id = 1:nindividuals)
+# df.p = rand(nindividuals)
+df.p .= 0.5
+transform!(df, :p => ByRow(p -> rand(Bernoulli(p), n)) => :success)
+
+barplot(df
+
+
+
+
+newdf = DataFrame(success = zeros(1), id = 99)
+μ = predict(m, newdf, new_re_levels=:population)
 
 nindividuals = 10
 n = 10
